3752899a089f007dd8568f6135c1d0c9635983f1
RaffaeleBerzoini <raffaele.berzoini@mail.polimi.it>
2021-12-13 20:13:27 +0100

    Script for dataset preparation

---
+++ prepare_dataset.py
@@ -0,0 +1,30 @@
+import os
+import zipfile
+
+
+def main(n, x):
+    dirpath = 'dataset/T_50/CT'
+
+    for dirs, dirnames, filenames in os.walk(dirpath):
+
+        filenames.sort()
+        for filename in filenames:
+
+            path = os.path.join(dirs, filename)
+            newpath = os.path.join(dirs, str(n) + '.dcm')
+
+            if filename.endswith(str(x) + '.dcm'):
+                os.rename(path, newpath)
+                x += 1
+                n += 1
+
+
+if __name__ == '__main__':
+    if os.path.exists('Resources.zip'):
+        print("Extracting zip folder")
+        zip_ref = zipfile.ZipFile('Resources.zip', 'r')
+        zip_ref.extractall()
+        zip_ref.close()
+        os.rename('Resources', 'dataset')
+    main(1, 1930)
+    main(53, 1846)
